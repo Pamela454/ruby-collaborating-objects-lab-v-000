900c23130a07e53d2b9d914e1e5b04bd59a86826
Pamela Frieze <pfrieze@gmail.com>
2017-11-19 23:21:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,15 +20,15 @@ class Artist
     @@all << self
   end
 
-  def self.find_or_create_by_name(name)    #defining a class method.
+  def self.find_or_create_by_name(name)    #defining a class method. Can access all instances stored. 
     self.find(name) ? self.find(name) : self.create(name)
   end
 
-  def self.create(name)     #defining a class method.
+  def self.create(name)     #defining a class method. Can access all instances stored.
     self.new(name).tap {|artist| artist.save}
   end
 
-  def self.find(name)      #defining a class method. 
+  def self.find(name)      #defining a class method. Can access all instances stored.
     self.all.find {|artist| artist.name == name }
   end
 
